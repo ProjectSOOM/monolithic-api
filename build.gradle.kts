f9c6d0e5def34649578932d4bb12dfff4b45f804
지인호 <77221262+key-del-jeeinho@users.noreply.github.com>
2022-04-01 21:11:24 +0900

    [ADD] 기존 api 소스코드를 참고하여 의존성, 테스크 등 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
     kotlin("jvm") version "1.6.10"
     kotlin("plugin.spring") version "1.6.10"
+    kotlin("plugin.jpa") version "1.6.10"
 }
 
 group = "com.soom"
@@ -13,13 +14,55 @@ java.sourceCompatibility = JavaVersion.VERSION_17
 
 repositories {
     mavenCentral()
+    maven { url = uri("https://www.jitpack.io" ) }
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter")
+    /////SPRING BOOT/////
+    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor") //configuration
+    implementation("org.springframework.boot:spring-boot-starter-web") //web
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa") //data jpa
+    implementation("org.springframework.boot:spring-boot-starter-data-redis") //redis
+    implementation("org.springframework.boot:spring-boot-starter-validation") //validation
+    implementation("org.springframework.boot:spring-boot-starter-security") //security
+    implementation("org.springframework.boot:spring-boot-starter-mail") //mailing
+    implementation("org.springframework.boot:spring-boot-starter-thymeleaf") //thymeleaf
+    implementation("org.springframework.boot:spring-boot-starter-actuator") //actuator
+    developmentOnly("org.springframework.boot:spring-boot-devtools") //devtool
+
+    /////KOTLIN/////
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.security:spring-security-test")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin") //jackson for kotilin
+
+    /////DATABASE/////
+    runtimeOnly("mysql:mysql-connector-java") //mysql
+
+    /////UTIL/////
+    implementation("com.squareup.okhttp3:okhttp:4.9.1") //okhttp
+    implementation("com.google.guava:guava:18.0") //guava
+    implementation("io.jsonwebtoken:jjwt:0.9.1") //jwt token
+
+    /////MESSAGING/////
+    implementation("com.corundumstudio.socketio:netty-socketio:1.7.19") //socket
+
+    /////PARSING/////
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.11.0") //jackson
+    implementation("com.fasterxml.jackson.core:jackson-annotations:2.11.0")
+    implementation("org.apache.tika:tika-parsers:2.2.1")//tika parser
+    implementation("org.apache.tika:tika-core:2.2.1")
+    implementation("io.github.leeseojune53:neis-api:1.0.3")//neisAPI
+    implementation("com.github.AppS01u7E:NeisApi:0.04.00")
+
+    /////MANAGING/////
+    implementation("org.springdoc:springdoc-openapi-webflux-ui:1.5.10") //openapi
+    implementation("org.apache.logging.log4j:log4j-api:2.17.0") //logging
+
+    /////INFRA/////
+    implementation("org.springframework.cloud:spring-cloud-starter-aws:2.2.6.RELEASE") //aws
+    implementation("com.google.firebase:firebase-admin:7.1.0") //firebase
 }
 
 tasks.withType<KotlinCompile> {
@@ -31,4 +74,15 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+allOpen {
+    annotation("javax.persistence.Entity")
+    annotation("javax.persistence.MappedSuperclass")
+    annotation("javax.persistence.Embeddable")
+}
+noArg {
+    annotation("javax.persistence.Entity")
+    annotation("javax.persistence.MappedSuperclass")
+    annotation("javax.persistence.Embeddable")
 }
