f03256ec57737b2d3e11e6803debfb4875e31c51
지인호 <77221262+key-del-jeeinho@users.noreply.github.com>
2022-04-02 16:14:21 +0900

    [ADD] 계정을 찾을 수 없을경우 발생하는 예외 작성

---
+++ src/main/kotlin/com/soom/monolithic_api/domain/account/exception/UnknownAccountException.kt
@@ -0,0 +1,5 @@
+package com.soom.monolithic_api.domain.account.exception
+
+class UnknownAccountException(val id: Long) : RuntimeException(message(id))
+
+private fun message(id: Long): String = "아이디가${id}인 계정을 찾을 수 없습니다!"
