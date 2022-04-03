2125f6dc67d6850203b0b3c59e7c498637aa895f
지인호 <77221262+key-del-jeeinho@users.noreply.github.com>
2022-04-03 13:30:10 +0900

    [FIX] delete 구현 및 scope function 과 copy 메서드를 통해서 기존 로직 리팩토링

--- src/main/kotlin/com/soom/monolithic_api/domain/account/service/AccountProfileImageService.kt
+++ src/main/kotlin/com/soom/monolithic_api/domain/account/service/AccountProfileImageService.kt
@@ -1,9 +1,10 @@
 package com.soom.monolithic_api.domain.account.service
 
+import com.soom.monolithic_api.domain.account.dto.AccountDto
 import com.soom.monolithic_api.domain.account.dto.ProfileImageDto
 import org.springframework.web.multipart.MultipartFile
 
 interface AccountProfileImageService {
     fun save(id: Long, image: MultipartFile): ProfileImageDto
-    fun delete(id: Long)
+    fun delete(id: Long): AccountDto
 }
--- src/main/kotlin/com/soom/monolithic_api/domain/account/service/AccountProfileImageServiceImpl.kt
+++ src/main/kotlin/com/soom/monolithic_api/domain/account/service/AccountProfileImageServiceImpl.kt
@@ -2,10 +2,10 @@ package com.soom.monolithic_api.domain.account.service
 
 import com.soom.monolithic_api.domain.account.dto.AccountDto
 import com.soom.monolithic_api.domain.account.dto.ProfileImageDto
+import com.soom.monolithic_api.domain.account.entity.AccountEntity
 import com.soom.monolithic_api.domain.account.entity.StudentEntity
 import com.soom.monolithic_api.domain.account.entity.TeacherEntity
-import com.soom.monolithic_api.domain.account.repository.StudentRepository
-import com.soom.monolithic_api.domain.account.repository.TeacherRepository
+import com.soom.monolithic_api.domain.account.repository.AccountRepository
 import com.soom.monolithic_api.domain.account.template.RepositoryAccountTemplate
 import com.soom.monolithic_api.infra.service.AwsS3Service
 import com.soom.monolithic_api.infra.type.S3DataType
@@ -16,34 +16,25 @@ import org.springframework.web.multipart.MultipartFile
 class AccountProfileImageServiceImpl(
     private val accountTemplate: RepositoryAccountTemplate,
     private val awsS3Service: AwsS3Service,
-    private val studentRepository: StudentRepository,
-    private val teacherRepository: TeacherRepository
+    private val accountRepository: AccountRepository
 ): AccountProfileImageService {
     override fun save(id: Long, image: MultipartFile): ProfileImageDto {
         val imageId = awsS3Service.upload(image, S3DataType.PROFILE_IMAGE.pathFormatter.invoke(arrayOf(id)))
-        val userId: Long = accountTemplate.doAndGetWithAccountById(id, changeTeacherProfile(imageId), changeStudentProfile(imageId)).meta.id
+        val userId: Long = accountTemplate.doAndGetWithAccountById(id, changeAccountProfile(imageId), changeAccountProfile(imageId)).meta.id
         return ProfileImageDto(userId, imageId)
     }
 
-    private fun changeStudentProfile(imageId: String): (StudentEntity) -> AccountDto {
-        return { entity ->
-            val new = StudentEntity(entity.id, entity.name, entity.gender, entity.birth,
-                entity.email, entity.encodedPassword, entity.role, entity.school, imageId,
-                entity.classNumber, entity.admissionAt, entity.department)
-            studentRepository.save(new)
-            new.toDto() }
-    }
+    override fun delete(id: Long): AccountDto = accountTemplate.doWithAccountById(id, deleteAccountProfile())
 
-    private fun changeTeacherProfile(imageId: String): (TeacherEntity) -> AccountDto {
-        return { entity ->
-            val new = TeacherEntity(entity.id, entity.name, entity.gender, entity.birth,
-                entity.email, entity.encodedPassword, entity.role, entity.school, imageId,
-                entity.major, entity.teacherType)
-            teacherRepository.save(new)
-            new.toDto() }
-    }
+    private fun deleteAccountProfile(): (AccountEntity) -> AccountDto = changeAccountProfile("")
 
-    override fun delete(id: Long) {
-        TODO("Not yet implemented")
+    private fun changeAccountProfile(imageId: String): (AccountEntity) -> AccountDto = { entity ->
+        when(entity) {
+            is StudentEntity -> entity.copy(profileImage = imageId)
+            is TeacherEntity -> entity.copy(profileImage = imageId)
+        }.run{
+            accountRepository.save(this)
+            this.toDto()
+        }
     }
 }
