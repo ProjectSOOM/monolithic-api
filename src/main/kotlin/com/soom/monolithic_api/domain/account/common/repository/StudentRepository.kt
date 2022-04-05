a1782463d87e591aec10c0005575bbf18ee69940
지인호 <77221262+key-del-jeeinho@users.noreply.github.com>
2022-04-05 17:02:59 +0900

    [ADD] fingByEmail 쿼리메서드 작성

--- src/main/kotlin/com/soom/monolithic_api/domain/account/common/repository/StudentRepository.kt
+++ src/main/kotlin/com/soom/monolithic_api/domain/account/common/repository/StudentRepository.kt
@@ -2,6 +2,9 @@ package com.soom.monolithic_api.domain.account.common.repository
 
 import com.soom.monolithic_api.domain.account.common.data.entity.StudentEntity
 import org.springframework.data.jpa.repository.JpaRepository
+import java.util.*
 
-interface StudentRepository : JpaRepository<StudentEntity, Long>
+interface StudentRepository : JpaRepository<StudentEntity, Long> {
+    fun findByEmail(email: String): Optional<StudentEntity>
+}
 
--- src/main/kotlin/com/soom/monolithic_api/domain/account/common/repository/TeacherRepository.kt
+++ src/main/kotlin/com/soom/monolithic_api/domain/account/common/repository/TeacherRepository.kt
@@ -2,5 +2,8 @@ package com.soom.monolithic_api.domain.account.common.repository
 
 import com.soom.monolithic_api.domain.account.common.data.entity.TeacherEntity
 import org.springframework.data.jpa.repository.JpaRepository
+import java.util.*
 
-interface TeacherRepository : JpaRepository<TeacherEntity, Long>
+interface TeacherRepository : JpaRepository<TeacherEntity, Long> {
+    fun findByEmail(email: String): Optional<TeacherEntity>
+}
