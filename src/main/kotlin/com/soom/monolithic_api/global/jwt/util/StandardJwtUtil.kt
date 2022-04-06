6ab5e6260d457a9c9b4bda77f02057dd1cb76969
지인호 <77221262+key-del-jeeinho@users.noreply.github.com>
2022-04-06 14:34:33 +0900

    [ADD] StandardJwtUtil 구현

---
+++ src/main/kotlin/com/soom/monolithic_api/global/jwt/util/StandardJwtUtil.kt
@@ -0,0 +1,34 @@
+package com.soom.monolithic_api.global.jwt.util
+
+import com.soom.monolithic_api.global.jwt.exception.WrongJwtTokenException
+import io.jsonwebtoken.Claims
+import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.SignatureAlgorithm
+import java.sql.Timestamp
+import java.time.LocalDateTime
+
+abstract class StandardJwtUtil<T>: JwtUtil<T> {
+    override fun encode(payload: T): String = LocalDateTime.now()
+        .let {
+        Jwts.builder().apply {
+            setClaims(getClaims(payload))
+            signWith(SignatureAlgorithm.HS256, getSecret())
+            setIssuedAt(Timestamp.valueOf(it))
+            setExpiration(Timestamp.valueOf(getExpiredAt(it)))
+        }.compact()
+    }
+
+    override fun decode(token: String): T =
+        runCatching {
+            Jwts.parser()
+                .setSigningKey(getSecret())
+                .parseClaimsJws(token)
+                .body.run(::getData)
+        }.onFailure { throw WrongJwtTokenException(token) }
+            .getOrThrow()
+
+    protected abstract fun getExpiredAt(now: LocalDateTime): LocalDateTime
+    protected abstract fun getSecret(): String
+    protected abstract fun getClaims(data: T): Claims
+    protected abstract fun getData(claims: Claims): T
+}
